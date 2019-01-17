359083d8dec52d37b1ed5dcbf29517cc10699158
David Sepulveda Ramirez <davidsepulvedaramirez@Davids-MacBook-Pro.local>
2019-01-16 23:44:57 -0500

    Stariting library

--- src/UbiMesh.cpp
+++ src/UbiMesh.cpp
@@ -1,1 +1,24 @@
 
+#include "UbiMesh.h"
+#include "UbiMeshConstants.h"
+#include "Particle.h"
+
+
+/**************************************************************************
+* Constructors
+***************************************************************************/
+UbiMesh::UbiMesh();
+
+/**************************************************************************
+* Functions to SEND data to the mesh network and then Ubidots
+***************************************************************************/
+bool UbiMesh::ubiMeshPublish(const char * event_name, const char * variable_label, float value){
+    char * name = (char *) malloc(sizeof(char) * MAX_NAME_SIZE);
+    char * value = (char *) malloc(sizeof(char) * MAX_DATA_SIZE);
+
+    sprintf(name, "%s/%s/%s", event_name, System.deviceID(), variable_label);
+    sprintf(value, "%f", value);
+
+    return Mesh.publish(name, value);
+
+}
---
+++ src/UbiMeshConstants.h
@@ -0,0 +1,11 @@
+#ifndef _UbiConstants_H_
+#define _UbiConstants_H_
+
+
+#include "Particle.h"
+
+const float ERROR_VALUE = -3.4028235E+8;
+const int MAX_NAME_SIZE = 63;
+const int MAX_DATA_SIZE = 255;
+
+#endif
